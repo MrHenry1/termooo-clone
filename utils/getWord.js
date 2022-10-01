06debc80f9d2a33dc1af445e00c15eabbd280e3a
J. Henry <52577797+MrHenry1@users.noreply.github.com>
2022-10-01 17:10:28 -0300

    Add files via upload

--- utils/getWord.js
+++ utils/getWord.js
@@ -1,7 +1,25 @@
-const words = ['CARRO', 'BARCO', 'CASCO', 'VACUO', 'MARCO', 'MARÇO', 'CASPA', 'VESPA', 'ZEBRA', 'TIRAR', 'TACAR', 'FARDO', 'JOGAR', 'GIRAR', 'LINHA', 'VINHA', 'DISCA', 'MUSGO', 'NAVIO', 'PORCO', 'POÇAO', 'QUERO', 'LESMA', 'ECOAR', 'RISAO', 'GOSMA', 'GARRA', 'FRUTA', 'GRUTA', 'CURAR', 'FIGOS', 'PALTA', 'SUIÇA', 'SAUNA', 'TANTO', 'VAZIO', 'DADOS', 'PRATO', 'FRACO', 'FORTE', 'BRUTO', 'GRITO', 'PORTE', 'PORTA', 'VILAO', 'VIGOR', 'DARDO', 'AVIAO', 'ASSAR', 'DISCO', 'CISCO', 'FIGOS', 'SONHO', 'BUSCA', 'FUSCA', 'BRITA', 'FRITA', 'CISMA', 'GATOS', 'RATOS', 'CALDA', 'CAUDA', 'FREVO', 'TREVO', 'GREGO', 'GRUTA', 'CAVAR']
+import latinize from 'latinize'
+import { words } from './words.js'
 
 function getWord() {
   return words[Math.floor(Math.random() * words.length)]
 }
 
-export { getWord }
+function checkWord(wordGiven) {
+  
+  if(words.indexOf(wordGiven) >= 0) {
+    return words[words.indexOf(wordGiven)]
+  }
+  
+  for(const word of words) {
+    if(latinize(word) == latinize(wordGiven)) {
+      
+      return word
+    }
+  }
+  
+  return null
+}
+
+export { getWord, checkWord }
+
